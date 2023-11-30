a928008ad3eb553f4e512c229f84cb1d7e85c99d
anav5704 <anavkki@gmail.com>
2023-11-30 13:13:57 +1200

    build: initialised elysia server

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia"
+import { html } from '@elysiajs/html'
+
+const app = new Elysia().use(html())
+
+app.get("/", ({ html }) => html(HTML_SHELL))
+
+app.listen(3000, () => console.log("Listening to Elysia on  port 3000"))
+
+const HTML_SHELL = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BETH todo apps</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
+`
+
