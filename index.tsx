9be2119a4e0567a3ddd708ca6c739d4b75b7ae01
anav5704 <anavkki@gmail.com>
2023-11-30 14:43:01 +1200

    feat: added todos on get

--- index.tsx
+++ index.tsx
@@ -4,18 +4,16 @@ import * as elements from "typed-html"
 
 const app = new Elysia().use(html())
 
-app.get("/", ({ html }) => 
-html(
-    <BaseHtml>
-      <body class="h-screen w-screen flex items-center justify-center flex-col gap-5">
-            <button class="border rounded-sm px-6 py-2" hx-post='/pizza' hx-swap="outerHTML">
-                Order pizza from Elysia server
-            </button>
-        </body>
-   </BaseHtml>
-))
+app.get("/", ({ html }) =>
+    html(
+        <BaseHtml>
+            <body hx-get="/todos" hx-swap="innerHTML" hx-trigger="load" class="h-screen w-screen flex items-center justify-center flex-col gap-5">
+              
+            </body>
+        </BaseHtml>
+    ))
 
-app.post("/pizza", () => <div class="text-6xl">🍕</div>)
+app.get("/todos", () => <ToDoList todos={db}/> )
 
 app.listen(3000, () => console.log("Listening to Elysia on  port 3000"))
 
@@ -32,3 +30,35 @@ const BaseHtml = ({ children }: elements.Children) => `
         ${children}
     </html>
 `
+
+type ToDo = {
+    id: number,
+    content: string,
+    completed: boolean,
+}
+
+const db: ToDo[] = [
+    { id: 1, content: "eat food", completed: false },
+    { id: 2, content: "drink water", completed: true }
+]
+
+const ToDoItem = ({ id, content, completed }: ToDo) => {
+    return (
+        <div class="flex flex-row space-x-5 items-center justify-end">
+            <p>{content}</p>
+            <input type="checkbox" checked={completed} id={id} />
+            <button class="border rounded-md px-6 py-1 bg-rose-500 text-white hover:bg-rose-600">Delete</button>
+        </div>
+    )
+}
+
+
+const ToDoList = ({ todos }: { todos: ToDo[] }) => {
+    return (
+        <div class="flex flex-col gap-y-3"> 
+            {todos.map((todo) => (
+                <ToDoItem {...todo} />
+            ))}
+        </div>
+    )
+}
